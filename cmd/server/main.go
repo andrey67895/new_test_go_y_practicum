9ff2bb26f39bec62b1d8e52bf637c9d79f9649a3
Andrey Guschin <andrey67895@gmail.com>
2024-03-29 01:25:05 +0500

    iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,83 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+var memLocalStorage = NewMemStorage()
+
+type MemStorage struct {
+	data map[string]string
+}
+
+func (e *MemStorage) Set(key string, value string) error {
+	e.data[key] = value
+	return nil
+}
+
+func (e *MemStorage) Get(key string) (any, error) {
+	value, ok := e.data[key]
+	if !ok {
+		return "", errors.New("key not found")
+	}
+	return value, nil
+}
+
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		data: make(map[string]string),
+	}
+}
+
+func JSONHandler(w http.ResponseWriter, req *http.Request) {
+	typeMet := req.PathValue("type")
+	nameMet := req.PathValue("name")
+	valueMet := req.PathValue("value")
+	if typeMet == "gauge" {
+		err := memLocalStorage.Set(nameMet, valueMet)
+		if err != nil {
+			return
+		}
+	} else if typeMet == "counter" {
+		localCounter, _ := memLocalStorage.Get(nameMet)
+		if localCounter == "" {
+			err := memLocalStorage.Set(nameMet, valueMet)
+			if err != nil {
+				return
+			}
+		} else {
+			inRouter, err := strconv.Atoi(valueMet)
+			local, err := strconv.Atoi(localCounter.(string))
+			if err != nil {
+				return
+			}
+			println(inRouter)
+			println(local)
+			value := strconv.Itoa(local + inRouter)
+
+			err = memLocalStorage.Set(nameMet, value)
+			if err != nil {
+				return
+			}
+		}
+	} else {
+		http.Error(w, "Неверный тип метрики! Допустимые значения: gauge, counter", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	localCounter, _ := memLocalStorage.Get(nameMet)
+	fmt.Println("Converted string:", localCounter)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/{type}/{name}/{value}", JSONHandler)
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/andrey67895/new_test_go_y_practicum
+
+go 1.22.1
