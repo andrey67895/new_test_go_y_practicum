c076caacfa489f33673898f35aa8631825a9086a
Andrey Guschin <andrey67895@gmail.com>
2024-03-30 01:18:45 +0500

    iter2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"runtime"
+)
+
+var HOST = "http://localhost:8080"
+
+func main() {
+	var memStats runtime.MemStats
+
+	runtime.ReadMemStats(&memStats)
+	fmt.Printf("Total allocated memory (in bytes): %d\n", memStats.Alloc)
+
+	//http.Post()
+	//r.Post("/update/{type}/{name}/{value}", handlers.MetHandler)
+	//err := http.ListenAndServe(":8080", r)
+	//if err != nil {
+	//	return
+	//}
+}
