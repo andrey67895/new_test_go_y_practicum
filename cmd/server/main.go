2bcc67b04fd317705ef5c5bb3bae356e7eb45832
Andrey Guschin <andrey67895@gmail.com>
2024-04-04 00:47:34 +0500

    iter4

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/andrey67895/new_test_go_y_practicum/internal/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	port := flag.Int("a", 8080, "port for host")
+	host := flag.String("a", "localhost:8080", "host for server")
+	port := strings.Split(*host, ":")[1]
 	flag.Parse()
 	r := chi.NewRouter()
 	r.Use(middleware.RealIP, middleware.Logger, middleware.Recoverer)
 	r.Post("/update/{type}/{name}/{value}", handlers.MetHandler)
 	r.Get("/value/{type}/{name}", handlers.GetMetHandler)
 	r.Get("/", handlers.GetAll)
-	err := http.ListenAndServe(":"+string(fmt.Sprintf("%d", *port)), r)
+	err := http.ListenAndServe(":"+port, r)
 	if err != nil {
 		println(err.Error())
 		return
