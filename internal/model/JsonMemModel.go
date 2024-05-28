43027293a6f322ab8c111e63abebcfe6f9502bea
Andrey Guschin <andrey67895@gmail.com>
2024-05-29 01:07:57 +0500

    iter9

--- internal/model/JsonMemModel.go
+++ internal/model/JsonMemModel.go
@@ -3,24 +3,8 @@ package model
 type JSONMetrics struct {
 	ID    string   `json:"id"`              // имя метрики
 	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
-	delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
-	value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
-}
-
-func (e *JSONMetrics) SetValue(v float64) {
-	e.value = &v
-}
-
-func (e *JSONMetrics) SetDelta(d int64) {
-	e.delta = &d
-}
-
-func (e *JSONMetrics) GetValue() float64 {
-	return *e.value
-}
-
-func (e *JSONMetrics) GetDelta() int64 {
-	return *e.delta
+	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
+	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
 }
 
 func (e *JSONMetrics) SetValue(v float64) {
