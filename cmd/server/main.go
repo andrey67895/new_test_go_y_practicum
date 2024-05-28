9bc3e7dbdb8df5abb231dc32ef2bea5282fba1b9
Andrey Guschin <andrey67895@gmail.com>
2024-05-29 00:22:37 +0500

    iter9

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -81,14 +81,14 @@ func SaveData(tModel model.JSONMetrics) {
 	nameMet := tModel.ID
 
 	if typeMet == "gauge" {
-		valueMet := tModel.GetValue()
+		valueMet := *tModel.Value
 		err := storage.LocalNewMemStorageGauge.SetGauge(nameMet, valueMet)
 		if err != nil {
 			println(err.Error())
 			return
 		}
 	} else if typeMet == "counter" {
-		valueMet := tModel.GetDelta()
+		valueMet := *tModel.Delta
 		localCounter, err := storage.LocalNewMemStorageCounter.GetCounter(nameMet)
 		if err != nil {
 			err := storage.LocalNewMemStorageCounter.SetCounter(nameMet, valueMet)
@@ -97,8 +97,9 @@ func SaveData(tModel model.JSONMetrics) {
 				return
 			}
 		} else {
-			tModel.SetDelta(localCounter + valueMet)
-			err = storage.LocalNewMemStorageCounter.SetCounter(nameMet, tModel.GetDelta())
+			sum := localCounter + valueMet
+			tModel.Delta = &sum
+			err = storage.LocalNewMemStorageCounter.SetCounter(nameMet, *tModel.Delta)
 			if err != nil {
 				println(err.Error())
 				return
@@ -114,14 +115,14 @@ func Save(fname string, storeInterval int) {
 		for k, v := range storage.LocalNewMemStorageGauge.GetData() {
 			tJSON := model.JSONMetrics{}
 			tJSON.ID = k
-			tJSON.SetValue(v)
+			tJSON.Value = &v
 			tJSON.MType = "gauge"
 			tModel = append(tModel, tJSON)
 		}
 		for k, v := range storage.LocalNewMemStorageCounter.GetData() {
 			tJSON := model.JSONMetrics{}
 			tJSON.ID = k
-			tJSON.SetDelta(v)
+			tJSON.Delta = &v
 			tJSON.MType = "counter"
 			tModel = append(tModel, tJSON)
 		}
