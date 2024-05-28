256ff7bae7dcea9666c9bc2c95e156c9a52a5ca8
Andrey Guschin <andrey67895@gmail.com>
2024-05-29 00:14:10 +0500

    Revert "iter9"
    This reverts commit 83e3954f1cf7acd889536caeaaebcec25a50b2c8.

--- internal/model/JsonMemModel.go
+++ internal/model/JsonMemModel.go
@@ -6,3 +6,19 @@ type JSONMetrics struct {
 	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
 	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
 }
+
+func (e *JSONMetrics) SetValue(v float64) {
+	e.Value = &v
+}
+
+func (e *JSONMetrics) SetDelta(d int64) {
+	e.Delta = &d
+}
+
+func (e *JSONMetrics) GetValue() float64 {
+	return *e.Value
+}
+
+func (e *JSONMetrics) GetDelta() int64 {
+	return *e.Delta
+}
