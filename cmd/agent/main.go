10a31afea1a6c01f07643a779888fd6e3178da75
Andrey Guschin <andrey67895@gmail.com>
2024-07-10 08:48:29 +0500

    iter15

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -6,9 +6,7 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
-	"math/rand"
 	"net/http"
-	"runtime"
 	"sync"
 	"time"
 
@@ -37,9 +35,7 @@ func updateMetrics(pollInterval time.Duration) {
 		wg.Add(2)
 		go func() {
 			defer wg.Done()
-			for _, statName := range metricsName {
-				metrics.SetDataMetrics(statName, model.NewGauge(statName, getMemByStats(statName)))
-			}
+			metrics.SetDataMetricsForMap(metricsName)
 		}()
 		go func() {
 			defer wg.Done()
@@ -183,66 +179,5 @@ func getMemByGopsutil() {
 	c, _ := cpu.Percent(0, true)
 	for i, percent := range c {
 		metrics.SetDataMetrics(fmt.Sprintf("CPUutilization%d", i+1), model.NewGauge(fmt.Sprintf("CPUutilization%d", i+1), percent))
-	}
-}
-
-func getMemByStats(name string) float64 {
-	var memStats runtime.MemStats
-	runtime.ReadMemStats(&memStats)
-	switch name {
-	case "Alloc":
-		return float64(memStats.Alloc)
-	case "BuckHashSys":
-		return float64(memStats.BuckHashSys)
-	case "Frees":
-		return float64(memStats.Frees)
-	case "GCCPUFraction":
-		return memStats.GCCPUFraction
-	case "GCSys":
-		return float64(memStats.GCSys)
-	case "HeapAlloc":
-		return float64(memStats.HeapAlloc)
-	case "HeapIdle":
-		return float64(memStats.HeapIdle)
-	case "HeapInuse":
-		return float64(memStats.HeapInuse)
-	case "HeapObjects":
-		return float64(memStats.HeapObjects)
-	case "HeapReleased":
-		return float64(memStats.HeapReleased)
-	case "HeapSys":
-		return float64(memStats.HeapSys)
-	case "LastGC":
-		return float64(memStats.LastGC)
-	case "Lookups":
-		return float64(memStats.Lookups)
-	case "MCacheInuse":
-		return float64(memStats.MCacheInuse)
-	case "MCacheSys":
-		return float64(memStats.MCacheSys)
-	case "MSpanInuse":
-		return float64(memStats.MSpanInuse)
-	case "MSpanSys":
-		return float64(memStats.MSpanSys)
-	case "Mallocs":
-		return float64(memStats.Mallocs)
-	case "NumForcedGC":
-		return float64(memStats.NumForcedGC)
-	case "NumGC":
-		return float64(memStats.NumGC)
-	case "OtherSys":
-		return float64(memStats.OtherSys)
-	case "PauseTotalNs":
-		return float64(memStats.PauseTotalNs)
-	case "StackInuse":
-		return float64(memStats.StackInuse)
-	case "Sys":
-		return float64(memStats.Sys)
-	case "TotalAlloc":
-		return float64(memStats.TotalAlloc)
-	case "RandomValue":
-		return rand.Float64()
-	default:
-		return 0
 	}
 }
--- internal/model/MetricsModel.go
+++ internal/model/MetricsModel.go
@@ -1,6 +1,10 @@
 package model
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/andrey67895/new_test_go_y_practicum/internal/helpers"
+)
 
 type Metrics struct {
 	mut  sync.RWMutex
@@ -11,6 +15,14 @@ func NewMetrics() *Metrics {
 	return &Metrics{
 		data: make(map[string]Gauge),
 	}
+}
+
+func (e *Metrics) SetDataMetricsForMap(metricsName []string) {
+	e.mut.Lock()
+	for _, statName := range metricsName {
+		e.data[statName] = NewGauge(statName, helpers.GetMemByStats(statName))
+	}
+	e.mut.Unlock()
 }
 
 func (e *Metrics) SetDataMetrics(key string, value Gauge) {
