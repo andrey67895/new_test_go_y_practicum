f3ce08937ce418925067237a46f92c55337f3269
andrey <andrey67895@gmail.com>
2024-08-24 21:25:07 +0500

    iter20

--- cmd/staticlint/multichecker.go
+++ cmd/staticlint/multichecker.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"go/ast"
-
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
 	"golang.org/x/tools/go/analysis/passes/appends"
@@ -53,9 +51,6 @@ import (
 	"golang.org/x/tools/go/analysis/passes/unusedresult"
 	"golang.org/x/tools/go/analysis/passes/unusedwrite"
 	"golang.org/x/tools/go/analysis/passes/usesgenerics"
-	"golang.org/x/tools/go/ast/inspector"
-	"honnef.co/go/tools/staticcheck"
-	"honnef.co/go/tools/stylecheck"
 )
 
 func main() {
@@ -108,79 +103,9 @@ func main() {
 		unusedresult.Analyzer,
 		unusedwrite.Analyzer,
 		usesgenerics.Analyzer,
-		ExitInMainAnalyzer,
-	}
-	for _, v := range staticcheck.Analyzers {
-		mychecks = append(mychecks, v.Analyzer)
-	}
-	for _, v := range stylecheck.Analyzers {
-		mychecks = append(mychecks, v.Analyzer)
 	}
 
 	multichecker.Main(
 		mychecks...,
 	)
-}
-
-var ExitInMainAnalyzer = &analysis.Analyzer{
-	Name:     "exitinmain",
-	Doc:      "check for os.Exit call in func main in package main",
-	Requires: []*analysis.Analyzer{inspect.Analyzer},
-	Run:      run,
-}
-
-func run(pass *analysis.Pass) (interface{}, error) {
-	const MAIN = "main"
-	const OS = "os"
-	const EXIT = "Exit"
-	isMainPkg := func(x *ast.File) bool {
-		return x.Name.Name == MAIN
-	}
-
-	isMainFunc := func(x *ast.FuncDecl) bool {
-		return x.Name.Name == MAIN
-	}
-
-	isOsExit := func(x *ast.SelectorExpr, isMain bool) bool {
-		if !isMain || x.X == nil {
-			return false
-		}
-		ident, ok := x.X.(*ast.Ident)
-		if !ok {
-			return false
-		}
-		if ident.Name == OS && x.Sel.Name == EXIT {
-			pass.Reportf(ident.NamePos, "os.Exit called in main func in main package")
-			return true
-		}
-		return false
-	}
-
-	i := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-	nodeFilter := []ast.Node{
-		(*ast.File)(nil),
-		(*ast.FuncDecl)(nil),
-		(*ast.SelectorExpr)(nil),
-	}
-	mainInspecting := false
-	i.Preorder(nodeFilter, func(n ast.Node) {
-		switch x := n.(type) {
-		case *ast.File:
-			if !isMainPkg(x) {
-				return
-			}
-		case *ast.FuncDecl:
-			f := isMainFunc(x)
-			if mainInspecting && !f {
-				mainInspecting = false
-				return
-			}
-			mainInspecting = f
-		case *ast.SelectorExpr:
-			if isOsExit(x, mainInspecting) {
-				return
-			}
-		}
-	})
-	return nil, nil
 }
--- go.mod
+++ go.mod
@@ -10,11 +10,9 @@ require (
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
 	golang.org/x/tools v0.24.0
-	honnef.co/go/tools v0.4.7
 )
 
 require (
-	github.com/BurntSushi/toml v1.4.1-0.20240526193622-a339e1f7089c // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
