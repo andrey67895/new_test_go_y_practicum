0a59e12ba5584d4363535271ea6bd138bc0ded23
andrey <andrey67895@gmail.com>
2024-08-24 20:30:50 +0500

    iter20

--- cmd/staticlint/multichecker.go
+++ cmd/staticlint/multichecker.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"go/ast"
+
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
 	"golang.org/x/tools/go/analysis/passes/appends"
@@ -51,6 +53,7 @@ import (
 	"golang.org/x/tools/go/analysis/passes/unusedresult"
 	"golang.org/x/tools/go/analysis/passes/unusedwrite"
 	"golang.org/x/tools/go/analysis/passes/usesgenerics"
+	"golang.org/x/tools/go/ast/inspector"
 	"honnef.co/go/tools/staticcheck"
 	"honnef.co/go/tools/stylecheck"
 )
@@ -105,6 +108,7 @@ func main() {
 		unusedresult.Analyzer,
 		unusedwrite.Analyzer,
 		usesgenerics.Analyzer,
+		ExitInMainAnalyzer,
 	}
 	for _, v := range staticcheck.Analyzers {
 		mychecks = append(mychecks, v.Analyzer)
@@ -116,4 +120,64 @@ func main() {
 	multichecker.Main(
 		mychecks...,
 	)
+}
+
+var ExitInMainAnalyzer = &analysis.Analyzer{
+	Name:     "exitinmain",
+	Doc:      "check for os.Exit call in func main in package main",
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+	Run:      run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	isMainPkg := func(x *ast.File) bool {
+		return x.Name.Name == "main"
+	}
+
+	isMainFunc := func(x *ast.FuncDecl) bool {
+		return x.Name.Name == "main"
+	}
+
+	isOsExit := func(x *ast.SelectorExpr, isMain bool) bool {
+		if !isMain || x.X == nil {
+			return false
+		}
+		ident, ok := x.X.(*ast.Ident)
+		if !ok {
+			return false
+		}
+		if ident.Name == "os" && x.Sel.Name == "Exit" {
+			pass.Reportf(ident.NamePos, "os.Exit called in main func in main package")
+			return true
+		}
+		return false
+	}
+
+	i := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	nodeFilter := []ast.Node{
+		(*ast.File)(nil),
+		(*ast.FuncDecl)(nil),
+		(*ast.SelectorExpr)(nil),
+	}
+	mainInspecting := false
+	i.Preorder(nodeFilter, func(n ast.Node) {
+		switch x := n.(type) {
+		case *ast.File:
+			if !isMainPkg(x) {
+				return
+			}
+		case *ast.FuncDecl:
+			f := isMainFunc(x)
+			if mainInspecting && !f {
+				mainInspecting = false
+				return
+			}
+			mainInspecting = f
+		case *ast.SelectorExpr:
+			if isOsExit(x, mainInspecting) {
+				return
+			}
+		}
+	})
+	return nil, nil
 }
