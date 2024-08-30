43488122a9f6e81f1400b7c00c0f5dc38b69b76f
andrey <andrey67895@gmail.com>
2024-08-30 19:38:26 +0500

    iter20

---
+++ internal/handlers/JsonHandler_test.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/andrey67895/new_test_go_y_practicum/internal/storage"
+)
+
+func TestGetPing(t *testing.T) {
+	type args struct {
+		iStorage storage.IStorageData
+	}
+	tests := []struct {
+		name string
+		args args
+		want http.HandlerFunc
+	}{
+		{
+			name: "positive test #1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodGet, "/ping", nil)
+			handler := GetPing(storage.InMemStorage{})
+			handler(w, req)
+			res := w.Result()
+			assert.Equal(t, http.StatusOK, res.StatusCode)
+		})
+	}
+}
